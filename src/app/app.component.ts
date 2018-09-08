e53897b3837b39cab9085714dbfc742d2e12d7d3
Unknown <macintosh.codeuse@gmail.com>
2018-09-08 15:29:15 +0200

    création du tableau des articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Blog';
+
+	// Date actuelle
+	date = new Date();
+
+	// Tableau des articles
+	posts = [
+		{
+			title: 'Article 1',
+			content: 'Je suis l\'article 1',
+			loveIts: 5,
+			created_at: this.date
+		},
+		{
+			title: 'Article 2',
+			content: 'Je suis l\'article 1',
+			loveIts: 0,
+			created_at: this.date
+		},
+		{
+			title: 'Article 3',
+			content: 'Je suis l\'article 1',
+			loveIts: -5,
+			created_at: this.date
+		}
+	];
 }
