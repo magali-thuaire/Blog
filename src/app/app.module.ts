d1edf423e2af6d4266388dff12b7a8b92da3a0d9
Unknown <macintosh.codeuse@gmail.com>
2018-09-09 10:47:36 +0200

    ajout du composant post-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		PostListComponent
 	],
 	imports: [
 		BrowserModule
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
